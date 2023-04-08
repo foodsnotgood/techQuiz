df67000b82be47a0c8b839e2d339d9ee1c3466e4
One16-JR <121863164+One16-JR@users.noreply.github.com>
2023-04-08 20:35:29 +0200

    convert json to list of questions

--- lib/dto/questionDto.dart
+++ lib/dto/questionDto.dart
@@ -1,18 +1,31 @@
 class QuestionDto {
   final String question;
-  final Map<String, String> answers;
+  final Map<dynamic, dynamic> answers;
   final String correctAnswer;
+  final Map<String, bool> correctAnswers;
+  final bool multipleCorrect;
 
   const QuestionDto(
       {required this.answers,
       required this.correctAnswer,
-      required this.question});
+      required this.question,
+      required this.correctAnswers,
+      required this.multipleCorrect});
 
-  factory QuestionDto.fromJson(Map<String, dynamic> json) {
+  factory QuestionDto.fromJson(Map<String, dynamic> data) {
+    // note the explicit cast to String
+    // this is required if robust lint rules are enabled
+    final question = data['question'] as String;
+    final correctAnswer = data['correct_answer'] ?? "";
+    final answers = data['answers'] as Map<dynamic, dynamic>;
+    final correctAnswers = data['correct_answers'] as Map<String, dynamic>;
+    final multipleCorrect = data['multiple_correct_answers'] as String;
     return QuestionDto(
-      answers: json['answers'],
-      correctAnswer: json['correct_answer'],
-      question: json['question'],
-    );
+        question: question,
+        answers: answers,
+        correctAnswers: correctAnswers.map((key, value) =>
+            MapEntry(key, value.toString().toLowerCase().trim() == 'true')),
+        correctAnswer: correctAnswer,
+        multipleCorrect: multipleCorrect.toLowerCase().trim() == 'true');
   }
 }
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,9 +1,65 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class Welcome extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'dto/questionDto.dart';
+
+class Welcome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  late Future<List<QuestionDto>> futureQuestions;
+
+  Future<List<QuestionDto>> fetchQuestions() async {
+    var response = await http.get(Uri.parse(
+        'https://quizapi.io/api/v1/questions?apiKey=ymuTwJq2H7AhPOR7CJkBY3WVaFrV0q09RirkWy2d&limit=5'));
+    print(json.decode(response.body)[0]['correct_answers'].runtimeType);
+    return (json.decode(response.body) as List)
+        .map((q) => QuestionDto.fromJson(q))
+        .toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureQuestions = fetchQuestions();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Questions here"),
+      ),
+      body: FutureBuilder<List<QuestionDto>>(
+          future: futureQuestions,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<QuestionDto> questions = snapshot.data as List<QuestionDto>;
+              return ListView.builder(
+                  itemCount: questions.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.all(8),
+                      padding: EdgeInsets.all(8),
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Text(questions[index].question),
+                        ],
+                      ),
+                    );
+                  });
+            }
+            if (snapshot.hasError) {
+              print(snapshot.error.toString());
+              return Text('error');
+            }
+            return Center(child: CircularProgressIndicator());
+          }),
+    );
   }
 }
