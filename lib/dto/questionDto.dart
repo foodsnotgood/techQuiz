61158738913790a1d5f063aed0ddcfe13bfebc5f
One16-JR <121863164+One16-JR@users.noreply.github.com>
2023-04-09 08:01:35 +0200

    mapping questions

--- lib/dto/questionDto.dart
+++ lib/dto/questionDto.dart
@@ -1,6 +1,6 @@
 class QuestionDto {
   final String question;
-  final Map<dynamic, dynamic> answers;
+  final Map<String, String> answers;
   final String correctAnswer;
   final Map<String, bool> correctAnswers;
   final bool multipleCorrect;
@@ -17,12 +17,12 @@ class QuestionDto {
     // this is required if robust lint rules are enabled
     final question = data['question'] as String;
     final correctAnswer = data['correct_answer'] ?? "";
-    final answers = data['answers'] as Map<dynamic, dynamic>;
+    final answers = data['answers'] as Map<String, dynamic>;
     final correctAnswers = data['correct_answers'] as Map<String, dynamic>;
     final multipleCorrect = data['multiple_correct_answers'] as String;
     return QuestionDto(
         question: question,
-        answers: answers,
+        answers: answers.map((key, value) => MapEntry(key, value ?? '')),
         correctAnswers: correctAnswers.map((key, value) =>
             MapEntry(key, value.toString().toLowerCase().trim() == 'true')),
         correctAnswer: correctAnswer,
