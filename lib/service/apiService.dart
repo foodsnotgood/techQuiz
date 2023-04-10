aa23f7a0196b88c9b3fd183ad8544f3d0202eb51
One16-JR <121863164+One16-JR@users.noreply.github.com>
2023-04-10 14:24:51 +0200

    remove comment

--- lib/service/apiService.dart
+++ lib/service/apiService.dart
@@ -8,8 +8,6 @@ class ApiService {
   String endpoint = dotenv.env['API_BASE_URL'] ?? '';
   String apiKey = dotenv.env['API_KEY'] ?? '';
 
-  // 'https://quizapi.io/api/v1/questions?apiKey=ymuTwJq2H7AhPOR7CJkBY3WVaFrV0q09RirkWy2d&limit=5';
-
   Future<List<QuestionDto>> fetchQuestions(String category) async {
     var response = await http
         .get(Uri.parse("$endpoint?apiKey=$apiKey&category=$category&limit=5"));
